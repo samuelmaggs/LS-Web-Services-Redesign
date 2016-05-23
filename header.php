3f6e7d08eccdfbd13dd741333bfd8bbf39eeb079
Samuel Maggs <samuelmaggs@yahoo.co.uk>
2016-05-23 19:33:54 +0100

    Initial Commit
    Website build for new LS Web Services website

---
+++ header.php
@@ -0,0 +1,14 @@
+<body>
+<header>
+  <nav>
+    <ul>
+      <li>Testimonials</li>
+      <li>Services</li>
+      <li>About</li>
+      <li><img class="logo" src="imgs/LSLogo.png" alt="LS Web Services Logo" /></li>
+      <li>Portfolio</li>
+      <li>Contact</li>
+      <li>Support</li>
+    </ul>
+  </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>LS Web Services | Website Design | Social Media </title>
+  <meta name="description" content="LS Web Services" />
+  <link rel="stylesheet" href="css/reset.css" charset="utf-8">
+  <link rel="stylesheet" href="css/style.css" charset="utf-8">
+</head>
+
+<?php include 'header.php'; ?>
+
+<div id="about" class="layer">
+  <div class="left-col">
+    <img src="" alt="Computer on desk" title="LS Web Services at work"/>
+  </div>
+  <div class="right-col">
+    <h1>About Us</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sagittis volutpat ultrices. Quisque ut dui molestie, posuere lacus at, efficitur leo. Proin pulvinar nisl eros, non volutpat dui pretium rhoncus. Vestibulum vel felis viverra, vulputate erat eget, venenatis massa. Cras felis eros, blandit in sodales viverra, blandit at magna. Donec sit amet finibus sapien. Suspendisse potenti. Fusce at vehicula augue. Donec lacinia, justo id rhoncus ullamcorper, diam arcu finibus ex, at porttitor sapien purus et orci. Mauris volutpat magna nunc. In sodales dui vel porta elementum. Sed quis turpis posuere velit sagittis bibendum. Nulla rhoncus non justo ac pharetra. In a ultrices justo. Ut egestas id mauris ac vestibulum.
+    </p>
+  </div>
+</div>
+<div id="services" class="layer">
+  <h1>Our Services</h1>
+  <div class="left-col">
+    <ul>
+      <li>Responsive Design</li>
+      <li>Social Media Management</li>
+      <li>Basic SEO Optimisation</li>
+    </ul>
+  </div>
+  <div class="right-col">
+    <ul>
+      <li>Responsive Design</li>
+      <li>Social Media Management</li>
+      <li>Basic SEO Optimisation</li>
+    </ul>
+  </div>
+</div>
+<div id="portfolio" class="layer">
+
+</div>
+<div id="contact" class="layer">
+  <div class="contact">
+    <img src="" alt="Email Us" title="Email us" />
+  </div>
+  <div class="contact">
+    <img src="" alt="Our social media" title="Contact us on social media" />
+  </div>
+  <div class="contact">
+    <img src="" alt="Call us" title="Call us" />
+  </div>
+</div>
+
+<?php include 'footer.php'; ?>
+</html>
